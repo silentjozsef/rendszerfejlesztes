678dc6b25b775a13f69a094565fc49f435f1ae14
Bálint <balint.gyorffy@gmail.com>
2022-04-05 23:28:44 +0200

    listByToolCategory query

--- app/src/main/java/com/example/androidapp/generic/query/EducationQuery.java
+++ app/src/main/java/com/example/androidapp/generic/query/EducationQuery.java
@@ -2,8 +2,15 @@ package com.example.androidapp.generic.query;
 
 import android.content.Context;
 
+import com.android.volley.Request;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.example.androidapp.generic.callback.ErrorCallBack;
+import com.example.androidapp.generic.callback.SuccessCallBack;
 import com.example.androidapp.generic.entity.Education;
 import com.example.androidapp.generic.entity.Tool;
+import com.example.androidapp.generic.response.GenericListResponse;
+
+import java.io.UnsupportedEncodingException;
 
 public class EducationQuery extends BaseQuery<Education> {
 
@@ -23,6 +30,31 @@ public class EducationQuery extends BaseQuery<Education> {
         super(url, context);
     }
 
+    public void listByToolCategory(Long toolCategoryId, final SuccessCallBack<GenericListResponse> successCallBack, final ErrorCallBack<GenericListResponse> errorCallBack) {
+        String requestUrl = backendURL + parentUrl + "list-by-tool-category?id=" + toolCategoryId;
+
+        String finalUrl = requestUrl;
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, requestUrl, null,
+                response -> {
+                    GenericListResponse genericSingleResponse = getGenericListResponse(response.toString());
+                    successCallBack.onSuccess(genericSingleResponse);
+                },
+                error -> {
+                    try {
+                        String body = null;
+                        GenericListResponse genericSingleResponse = null;
+                        if (error.networkResponse != null && error.networkResponse.data != null) {
+                            body = new String(error.networkResponse.data, "UTF-8");
+                            genericSingleResponse = getGenericListResponse(body);
+                        }
+                        errorCallBack.onError(genericSingleResponse);
+                    } catch (UnsupportedEncodingException e) {
+                        e.printStackTrace();
+                    }
+                }
+        );
+        sendRequest(request);
+    }
 
 
     @Override
